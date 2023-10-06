b17264c06b92b4031e2d9985c6aa4600ce3fda50
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-06 08:56:07 +0530

    Add files via upload

---
+++ JS_Assignment/SingleToTwoString.js
@@ -0,0 +1,16 @@
+// 3] . Write a program to get a single string from two given strings, separated by a space and swap the first two
+// characters of each string.
+
+
+
+let arr1="aniket,Kusundal"
+   s= arr1.split(",")
+    console.log(s[0])
+    console.log(s[1])
+
+
+
+// let arr1="aniket,Kusundal"
+//    s= arr1.replace("aniket")
+//     console.log(s[0])
+//     console.log(s[1])
---
+++ JS_Assignment/StringEndsWith.js
@@ -0,0 +1,18 @@
+// 2].  Write a program to add 'ing' at the end of a given string (length should be at least 3). If the given string
+// already ends with 'ing' then add 'ly' instead. If the string length of the given string is less than 3, leave it
+// unchanged.
+
+
+
+let s1="kbc";
+
+if(s1.endsWith("ing")){
+    console.log(s1+"ly")
+    
+}
+else{
+    console.log(s1+"ing")
+
+}
+
+
