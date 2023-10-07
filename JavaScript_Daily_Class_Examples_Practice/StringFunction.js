e9ba39279e9b0ca62b366a2a71a10d3554687285
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-07 09:29:17 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/StringFunction.js
@@ -0,0 +1,104 @@
+
+
+// let s1 = "This is Double Quote"
+// let s2 = 'This is Single Quote'
+// let s3 = `this is backticks`
+
+//     console.log(s1);
+//     console.log(s2);
+//     console.log(s3);
+
+
+
+
+// toLocaleUpperCase()
+    // let str = "aniket kusundal"
+    // console.log(str.toLocaleUpperCase);
+
+    // toLocaleLowerCase()
+    // let str1 = "ANIKET KUSUNDAL"
+    // console.log(str1.toLocaleLowerCase);
+
+
+    // indexOf()
+    // let str2 = "ANIKET KUSUNDAL"
+    // let a = str2.indexOf("T")
+    // console.log(a);
+
+    // lastIndexOf()
+    // let str2 = "ANIKET KUSUNDAL"
+    // let a = str2.lastIndexOf("L")
+    // console.log(a);
+
+
+
+    // startsWith()
+    // let str2 = "aniket kusundal"
+    // console.log(str2.startsWith("b"));
+    // console.log(str2.startsWith("an"));
+    // console.log(str2.startsWith("ket"));
+
+
+
+    // endsWith()
+    // let str2 = "aniket kusundal"
+    // console.log(str2.endsWith("dal"));
+    // console.log(str2.endsWith("ku"));
+    // console.log(str2.endsWith("l"));
+
+
+    // includes()
+    // let str2 = "aniket kusundal"
+
+    // let a = str2.includes("nik")
+    // console.log(a);
+
+
+    // slice()
+
+    //  let str2 = "aniket kusundal"
+    //     let str=str2.slice(2,9)
+    //  console.log(str);
+
+
+    // substring()
+
+    // let str2 = "aniket kusundal"
+    // let str=str2.substring(2,8)
+    // console.log(str);
+
+
+//  diffrance b/w slice and SubStroing
+    // let str2 = "aniket kusundal"
+
+    // let  str = str2.slice(-4,-1)  //---> return the substring bw start and end Index negative indexing Also Support 
+    // console.log(str);
+
+    // let str1 = str2.substring(-2,-1);    //---> it is similar to slice but allow start  index to be grator than end index in this casse it simply swap start and end index
+
+
+    // console.log(str2);
+
+    // console.log("-------------------------------------------");
+
+    // let s = str2.slice(6,2);
+    // console.log(s);
+
+
+    // let s2 = str2.substring(6,2)
+    // console.log(s2);
+
+
+
+
+    // charCodeAt()  -> it gives the ASci Values 
+
+    let char = "AZCDEF"
+    console.log(char.charCodeAt(0));
+    console.log(char.charCodeAt(1));
+    console.log("last");
+    console.log(String.fromCodePoint(1));
+
+
+
+     
