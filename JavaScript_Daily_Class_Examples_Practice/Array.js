fa1979098dd238671d217f98e2af60a44f012890
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-07 09:28:46 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/Array.js
@@ -0,0 +1,65 @@
+//  ARRAY  --> array is a collection of same item item may be same type of diffrent type
+
+        //  we can create array by two way
+
+    // [1]  --> By Using Square Bracket
+    // let arr = [10,20,30,40,"abc" , "XYZ"]
+    // console.log(arr);
+
+
+    // [2] ---> by using new keyWord 
+
+        // let arr = new Array (1,2,3,"XYZ")
+        // console.log(arr);
+
+
+
+//  HOW    TO      ACCESS       ARRAY      ELEMENT 
+
+// 
+
+
+
+
+//    THERE ARE THREE  WAYS TO ACCESS THE ARRAY ELEMENT
+
+//   [1] --->  BY USING  SUQARE    BRACKET  AND INDEX
+
+// let arr = [10,20,30,40,"abc" , "XYZ"]
+//     console.log(arr[0]);
+//     console.log(arr[-1]);    // not supporting negative indexing
+//     console.log(arr[5]);
+
+
+
+//  [2]  BY USING AT FUNCTION
+
+// let arr = [10,20,30,40,"abc" , "XYZ"]
+
+// console.log(arr.at(0));
+// console.log(arr.at(1));
+
+
+let arr = [10,20,30,40,"abc" , "XYZ"]
+
+// console.log('by using for loop');
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+
+
+
+// console.log("by using for of loop");
+
+// for (const x of arr) {
+//     console.log(x);
+    
+// }
+
+
+// console.log("by using for each");
+
+// arr.forEach((n)=>console.log(n))
+
