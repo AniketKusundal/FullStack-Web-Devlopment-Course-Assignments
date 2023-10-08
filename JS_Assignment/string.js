21ef88e56ef1745692e016bf28376b6548bb02a6
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-08 22:10:54 +0530

    Add files via upload

---
+++ JS_Assignment/string.js
@@ -0,0 +1,15 @@
+// 4. Write a program to get a string made of the first 2 and the last 2 chars from a given a string. If the string
+// length is less than 2, return instead of the empty string
+
+
+
+let str ="Aniket kusundal"
+
+
+    let s1=str.slice(0,3) + str.slice(str.length-2,str.length)
+
+    console.log(s1);
+    // console.log(str);
+
+
+// copleted
