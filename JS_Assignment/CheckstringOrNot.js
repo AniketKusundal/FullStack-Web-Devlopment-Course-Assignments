9c1f7515ebfac327ee94ba09354e0b66f049ad5b
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-08 22:10:20 +0530

    Add files via upload

---
+++ JS_Assignment/CheckstringOrNot.js
@@ -0,0 +1,20 @@
+// 5)Write a JavaScript funcion to check whether an `input` is a string or not. Go to the editor
+
+
+   let a = 123
+
+   console.log(typeof a);
+   
+   let x=typeof a
+   if(x=='string'){
+
+      console.log("is a string");
+   }
+
+   else{
+      console.log("not a string");
+   }
+
+
+
+   // complted
