aed30b3a3ad0e3a58d304ade93d5f32dfa19a504
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-11 21:53:29 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/Array_Function.js
@@ -0,0 +1,88 @@
+//  Array Function 
+
+// [1]    PUSH()
+// [2]    POP()
+// [3]    SHIFT()
+// [4]    UNSHIFT()
+
+
+
+let arr = [10,20,30,40]
+
+// add item to Array
+
+
+
+// Push And POP
+
+// console.log(arr);
+// arr.push(50)
+// arr.push(60)
+// console.log(arr);
+
+
+// arr.pop(60)
+// console.log(arr);
+
+
+
+
+
+// Shift And unshift
+
+// console.log(arr);
+// arr.unshift(1)
+// console.log(arr);
+
+
+// arr.shift(10)
+// console.log(arr);
+
+
+
+
+// -------------------------------------------------------------------------------------------------------\\
+// -------------------------------------------------------------------------------------------------------\\
+// -------------------------------------------------------------------------------------------------------\\
+
+
+
+//  CONCAT
+
+// let arr  = [10,20,30]
+// let arr1 = [40,50,60]
+
+// let arr2 = arr.concat(arr1)
+// console.log(arr2);
+
+
+
+
+// let arr = [10,20,30,40,50,60,70,80,90,100]
+
+
+// SLCIE()
+
+// let arr2=arr.slice(1,5)
+// console.log(arr2);
+
+
+//  indexOF
+
+// let x = arr.indexOf(50)
+// console.log(x);
+
+
+// lastIndexOf()
+
+// let x = arr.lastIndexOf(70)
+// console.log(x);
+
+
+
+// includes()
+
+// let y = arr.includes(100)
+// console.log(y);
+                                            
+
