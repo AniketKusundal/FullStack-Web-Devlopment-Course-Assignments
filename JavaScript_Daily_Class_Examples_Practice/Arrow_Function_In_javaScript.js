ff38105d09ab568dfc16a61464c3dd8bfb95e944
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-11 21:55:01 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/Arrow_Function_In_javaScript.js
@@ -0,0 +1,44 @@
+// ARROW FUNCTION IN JAVASCRIPT ---- > > >   it is a shortcut way to create function there are two arrow function
+
+                // 1] --->  single line arrow function
+                // 2] --->  Multiline line arrow function
+
+
+
+
+    //  syntax
+    // 1] --->  single line arrow function
+    // (1)  let variable =(args)=> expression
+
+
+    //  syntax
+    // 2] --->  Multiline line arrow function
+
+    // let variable=(args)=>{
+            // -----------------------
+            // ----------------------
+            //------------------------
+    // }
+
+
+//     function add(x,y) {
+//         let z = x+y;
+//         return z
+//     }
+//     add(10,9)
+    
+
+//  example 2
+
+        // let add2 = (x,y)=>{
+        //         let z=x+y
+        //         return z
+        // }
+
+        // let a = add2 (100,50)              
+        // let b = add2 (100,50)        
+        // let c = add2 (100,50)        
+    
+        // console.log(a);
+        // console.log(b);        
+        // console.log(c);
