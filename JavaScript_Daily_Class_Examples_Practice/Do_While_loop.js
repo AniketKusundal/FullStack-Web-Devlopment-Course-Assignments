b9a155f5d9d13c9dabcff66ff157431365246954
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-13 21:07:40 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/Do_While_loop.js
@@ -0,0 +1,23 @@
+//  Do While Loop in JavaScript
+// let i=12
+
+// do{
+//     console.log(i);
+//     i = i+1;
+
+// }
+// while(i<=10)
+
+
+
+// While Loop In JavaScript
+
+let i = 10
+while (i<=10) {
+    
+    console.log(i);
+    i=i+1;
+}
+
+
+console.log(i);
