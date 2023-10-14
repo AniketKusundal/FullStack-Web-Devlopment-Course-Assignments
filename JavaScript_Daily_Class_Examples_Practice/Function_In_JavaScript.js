32ffd82a1bec9bf1476c62648960a85ee87f792a
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-14 22:18:40 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/Function_In_JavaScript.js
@@ -0,0 +1,133 @@
+// Function Defination
+
+// function sayHello() {
+    
+//     console.log("hello Aniket");
+//     console.log("My First Function ");
+// }
+
+
+
+//  Call To Function
+
+// console.log("Before");
+// sayHello()
+// console.log("after");
+
+
+
+
+// FUNCTION    WITH    ARGUMENT  
+
+// *****  EXAMPLE   2  *****
+
+// function sayHello(fname) {
+//     console.log("hello" + fname);
+// }
+
+// function add(x,y) {
+//     let z=x+y
+//     console.log("Addition Is " + z);
+// }
+
+
+// sayHello("aniket")
+// sayHello("kusundal ")
+// add(99,1)
+
+
+
+
+
+
+
+//  DEFULAT ARGUMENT VALUE
+// *****  EXAMPLE   3  *****
+
+// function say(fname="Anii") {
+//     console.log("Hello" +fname);
+
+// }
+
+
+
+// function add(x=10,y=90) {
+//     let z=x+y;
+//     console.log("Addition Is  "+z);
+// }
+
+
+// say()
+// say("hello")
+// add()
+// add(10,20)
+
+
+
+
+
+
+
+
+
+
+
+//   RETURNING VALUE FUNCTION
+
+// function getmsg(fname) {
+    
+//     let msg = "Hello " + fname
+//     return msg;
+// }
+
+
+
+// function area(r) {
+    
+//     let a = 3.14*r*r
+
+//     return a
+// }
+
+
+
+// let s1 = getmsg("abc")
+// console.log(s1);
+
+// let x = area(1.2)
+// console.log("Area iS "+x);
+
+
+
+
+
+
+
+//  FUNCTION  EXPRESSON
+
+// function show() {
+//     console.log("Hello");
+// }
+
+
+
+// let fun1 = function(){
+//     console.log("Aniket");
+// }
+
+
+// let add = function (x,y){
+
+//     let z=x+y
+//     return z
+// }
+
+// show()
+// fun1()
+
+
+
+// let a = add(10,1)
+// console.log("Addition Is "+a);
+
+
