814ac2dc502764fbed6989b8dce84cac763f8c89
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-14 22:17:07 +0530

    Add files via upload

---
+++ JavaScript_Daily_Class_Examples_Practice/Find_function.js
@@ -0,0 +1,12 @@
+// find() --> it is used to find the object of array find the function arrow function as an argument 
+    // If Not found data its output is Undefined 
+
+    // let arr = [
+    //     {id:100,fname:"aniket"},
+    //     {id:200,fname:"jack"},
+    //     {id:200,fname:"jack"},
+    //     {id:300,fname:"jhon"}
+    // ]
+
+    // let obj = arr.find((item)=>item.id=200)
+    // console.log(obj);
