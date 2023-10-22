6dc995bfc7657f998f6440b61f06a99f9e8444af
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-10-22 21:36:49 +0530

    Add files via upload

---
+++ JS_Assignment/07-Array-ToString.js
@@ -0,0 +1,21 @@
+// 7) 1.Write a simple JavaScript program to join all elements of the following array into a string.
+// Go to the editorSample array :
+
+
+
+// myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :"Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
+
+
+
+
+let myColor = ["Red", "Green", "White", "Black"];
+
+console.log(myColor.toString());
+console.log(myColor.join(" "));
+console.log(myColor.join(""));
+
+
+// completed
