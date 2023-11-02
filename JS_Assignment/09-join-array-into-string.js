40976663d06ab17c062be79b334fab214e46def9
Aniket Kusundal <112200178+AniketKusundal@users.noreply.github.com>
2023-11-02 21:25:02 +0530

    Add files via upload

---
+++ JS_Assignment/09-join-array-into-string.js
@@ -0,0 +1,14 @@
+// 9) write s simple javascript program to join all elemnts of the following arry into string
+
+
+
+let arr=[1,2,3,4,5,"aniket","kusundal","hello"]
+
+console.log("Array Element");
+console.log(arr);
+
+x=arr.toString()
+console.log("Array To String");
+console.log(x);
+
+
